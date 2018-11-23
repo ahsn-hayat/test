339e3e9b9297da77ac4bf929483fbf1a9449eaab
Ahsan <ahsan.hayat@xeffectstudio.com>
2018-11-23 18:09:44 +0500

    This is my second commit with changes in MainActivity

--- app/src/main/java/com/ahsan/gitapplicationdemo/MainActivity.java
+++ app/src/main/java/com/ahsan/gitapplicationdemo/MainActivity.java
@@ -6,12 +6,19 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FragmentOne fragmentOne;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        fragmentOne = new FragmentOne();
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragment_container, fragmentOne)
+                .commit();
+    }
 
     @Override
     protected void onResume() {
